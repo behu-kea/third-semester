1c7ba91225ac31235c39c132fd5a55d51c39f431
Benjamin Hughes <behu@kea.dk>
2021-11-11 14:06:36 +0100

    asd

--- code-written/apis/main.js
+++ code-written/apis/main.js
@@ -1,6 +1,18 @@
+/*
 fetch("http://api.open-notify.org/astros.json")
   .then((Response) => Response.json())
   .then((astronautsData) => {
     const pTag = document.querySelector("p");
     pTag.innerHTML = astronautsData.number;
   });
+*/
+
+fetch("http://localhost:8080/users", {
+  method: "POST",
+  body: JSON.stringify({ name: "Marianne", age: 77 }),
+  headers: {
+    "Content-type": "application/json; charset=UTF-8",
+  },
+})
+  .then((Response) => Response.json())
+  .then((users) => {});
