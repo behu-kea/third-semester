1435bfc57bd58f299cc7475c589df67f6f35e77a
Benjamin Hughes <behu@kea.dk>
2021-11-11 13:18:57 +0100

    asd

--- code-written/apis/main.js
+++ code-written/apis/main.js
@@ -1,43 +1,6 @@
-/*
-fetch("http://localhost:8080/users")
+fetch("http://api.open-notify.org/astros.json")
   .then((Response) => Response.json())
-  .then((users) => {
-    console.log(users);
+  .then((astronautsData) => {
+    const pTag = document.querySelector("p");
+    pTag.innerHTML = astronautsData.number;
   });
-
-*/
-fetch("http://localhost:8080/users", {
-  method: "POST",
-  body: JSON.stringify({ name: "Hans", age: 44 }),
-  headers: {
-    "Content-type": "application/json; charset=UTF-8",
-  },
-})
-  .then((Response) => Response.json())
-  .then((users) => {
-    console.log(users);
-    hej = 1;
-  });
-
-/*
-
-var myHeaders = new Headers();
-myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
-myHeaders.append("Cookie", "Cookie_1=value");
-
-var urlencoded = new URLSearchParams();
-urlencoded.append("name", "yrsula");
-urlencoded.append("age", "34");
-
-var requestOptions = {
-  method: "GET",
-  headers: myHeaders,
-  body: urlencoded,
-  redirect: "follow",
-};
-
-fetch("http://localhost:8080/users", requestOptions)
-  .then((response) => response.text())
-  .then((result) => console.log(result))
-  .catch((error) => console.log("error", error));
-*/
